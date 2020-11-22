80f48e7147a17756067d9301f6f2aacdc6a417d4
Jesús González Álvarez <51116828+JesusGonzalezA@users.noreply.github.com>
2020-11-22 17:41:21 +0100

    Routing de las páginas menu de la web

--- WebVale/src/app/app-routing.module.ts
+++ WebVale/src/app/app-routing.module.ts
@@ -1,7 +1,38 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ActividadesPageComponent } from './components/pages/actividades-page/actividades-page.component';
+import { CentroPageComponent } from './components/pages/centro-page/centro-page.component';
+import { LoginPageComponent } from './components/pages/login-page/login-page.component';
+import { ObjetivosPageComponent } from './components/pages/objetivos-page/objetivos-page.component';
+import { PerfilPageComponent } from './components/pages/perfil-page/perfil-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/login',
+    pathMatch: 'full'
+  },
+  {
+    path: 'login',
+    component: LoginPageComponent
+  },
+  {
+    path: 'objetivos',
+    component: ObjetivosPageComponent
+  },
+  {
+    path: 'actividades',
+    component: ActividadesPageComponent
+  },
+  {
+    path: 'perfil',
+    component: PerfilPageComponent
+  },
+  {
+    path: 'centro',
+    component: CentroPageComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
