25c27cf6a6f26ae2401029bb38d15cf7f4fc5080
Ana <anamaria.novoa@rai.usc.es>
2020-11-25 23:14:50 +0100

    Update facilitadores.service.ts

--- WebVale/src/app/services/facilitadores.service.ts
+++ WebVale/src/app/services/facilitadores.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FacilitadoresService {
 
-  constructor() { }
+  API_URI = 'http://localhost:3000/api/vale/facilitadores';
+
+  constructor(private http: HttpClient) {}
+
+  getObjetivos() {
+    return this.http.get(`${this.API_URI}/`)
+  }
 }
