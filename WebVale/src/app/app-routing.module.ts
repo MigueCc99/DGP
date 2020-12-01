0758ef5b2d1125bd7f6e776b7c0fed3da0cf014b
Jesús González Álvarez <51116828+JesusGonzalezA@users.noreply.github.com>
2020-12-01 11:12:02 +0100

    Recuperar contraseña de facilitadores terminado

--- WebVale/src/app/app-routing.module.ts
+++ WebVale/src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { ObjetivoFormComponent } from './components/forms/objetivo-form/objetivo
 import { ActividadInfoPageComponent } from './components/pages/actividad-info-page/actividad-info-page.component';
 import { ActividadesPageComponent } from './components/pages/actividades-page/actividades-page.component';
 import { CentroPageComponent } from './components/pages/centro-page/centro-page.component';
+import { FacilitadoresPageComponent } from './components/pages/facilitadores-page/facilitadores-page.component';
 import { LoginPageComponent } from './components/pages/login-page/login-page.component';
 import { ObjetivoInfoPageComponent } from './components/pages/objetivo-info-page/objetivo-info-page.component';
 import { ObjetivosPageComponent } from './components/pages/objetivos-page/objetivos-page.component';
@@ -57,6 +58,10 @@ const routes: Routes = [
   {
     path: 'centro',
     component: CentroPageComponent
+  },
+  {
+    path: 'centro/recuperar-contra/facilitadores',
+    component: FacilitadoresPageComponent
   },
   {
     path: 'socios',
