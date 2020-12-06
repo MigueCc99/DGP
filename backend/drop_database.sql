cfb338bd4221edffb191504f8e32c364f7010e6d
Jesús González Álvarez <51116828+JesusGonzalezA@users.noreply.github.com>
2020-12-06 16:27:44 +0100

    Script para eliminar las tablas

---
+++ backend/drop_database.sql
@@ -0,0 +1,7 @@
+USE vale;
+DROP table facilitadores;
+drop table socios;
+drop table objetivos;
+drop table actividades;
+drop table actividad_asignada_socio;
+drop table actividad_pertenece_objetivo;
