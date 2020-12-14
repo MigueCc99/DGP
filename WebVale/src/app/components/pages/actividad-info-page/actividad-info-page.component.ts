8bb77866c106e9b34d20ae8461b7f9832d4f1609
Ana <anamaria.novoa@rai.usc.es>
2020-12-14 18:38:33 +0100

    Diagrama añadido en actividad

--- WebVale/src/app/components/pages/actividad-info-page/actividad-info-page.component.ts
+++ WebVale/src/app/components/pages/actividad-info-page/actividad-info-page.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import {Location} from '@angular/common';
+import * as am4core from "@amcharts/amcharts4/core";
+import * as am4charts from "@amcharts/amcharts4/charts";
+
 import { Actividad } from 'src/app/models/Actividad';
 import { ActividadesService } from 'src/app/services/actividades.service';
 import { SociosService } from 'src/app/services/socios.service';
@@ -17,6 +20,7 @@ export class ActividadInfoPageComponent implements OnInit {
   actividad : Actividad = new Actividad();
   idActividad : number = 0;
   feedback : Feedback = new Feedback();
+  
 
 
   constructor(private actividadesService : ActividadesService, private _location: Location, private activeRoute : ActivatedRoute) {
@@ -29,6 +33,9 @@ export class ActividadInfoPageComponent implements OnInit {
   async Actualizar() : Promise<void> {
     let params = this.activeRoute.snapshot.params;
     this.idActividad = params.id;
+
+    // Create chart instance
+    let chart = am4core.create("chartdiv", am4charts.PieChart);
 
     // Obtener actividad
     this.actividadesService.getActividad(this.idActividad)
@@ -44,9 +51,26 @@ export class ActividadInfoPageComponent implements OnInit {
     .subscribe(
       res => {
         this.feedback = res as Feedback;
+
+        // Add data
+        chart.data = [{
+          "feedback": "Útil",
+          "votos": this.feedback.utiles
+        }, {
+          "feedback": "Difícil",
+          "votos": this.feedback.dificiles
+        }, {
+          "feedback": "Ha gustado",
+          "votos": this.feedback.gustados
+        }];
       },
       err => console.error(err)
     );
+
+    // Add and configure Series
+    let pieSeries = chart.series.push(new am4charts.PieSeries());
+    pieSeries.dataFields.value = "votos";
+    pieSeries.dataFields.category = "feedback";
   }
 
   showFeedback() : boolean{
